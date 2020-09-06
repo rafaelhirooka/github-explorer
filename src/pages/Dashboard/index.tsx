c35db0658b6fd7966f0023501a6f7338f79830ba
Rafael Hirooka <rafaelhirooka16@gmail.com>
2020-09-06 15:56:27 -0300

    dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/28057280?s=460&u=e9abff7e74551503b72f4b491e2d682389dc6f38&v=4"
+            alt="Rafael Hirooka"
+          />
+          <div>
+            <strong>rafaelhirooka/gobarber</strong>
+            <p>Blalblablalbla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/28057280?s=460&u=e9abff7e74551503b72f4b491e2d682389dc6f38&v=4"
+            alt="Rafael Hirooka"
+          />
+          <div>
+            <strong>rafaelhirooka/gobarber</strong>
+            <p>Blalblablalbla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/28057280?s=460&u=e9abff7e74551503b72f4b491e2d682389dc6f38&v=4"
+            alt="Rafael Hirooka"
+          />
+          <div>
+            <strong>rafaelhirooka/gobarber</strong>
+            <p>Blalblablalbla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
